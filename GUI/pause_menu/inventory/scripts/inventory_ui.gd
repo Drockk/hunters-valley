d4e89d9f755a33f51a439a49188e2a4961786998
Bartosz Zielonka <batziell@gmail.com>
2025-04-26 17:50:36 +0200

    Pickup and use items

--- GUI/pause_menu/inventory/scripts/inventory_ui.gd
+++ GUI/pause_menu/inventory/scripts/inventory_ui.gd
@@ -2,6 +2,8 @@ class_name InventoryUI extends Control
 
 const INVENTORY_SLOT = preload("res://GUI/pause_menu/inventory/inventory_slot.tscn")
 
+var focus_index: int = 0
+
 @export var data: InventoryData
 
 
@@ -9,6 +11,7 @@ func _ready() -> void:
 	PauseMenu.shown.connect(update_inventory)
 	PauseMenu.hidden.connect(clear_inventory)
 	clear_inventory()
+	data.changed.connect(on_inventory_changed)
 	pass
 
 
@@ -17,10 +20,27 @@ func clear_inventory() -> void:
 		c.queue_free()
 
 
-func update_inventory() -> void:
+func update_inventory(i: int = 0) -> void:
 	for s in data.slots:
 		var new_slot = INVENTORY_SLOT.instantiate()
 		add_child(new_slot)
 		new_slot.slot_data = s
+		new_slot.focus_entered.connect(item_focus)
 
-	get_child(0).grab_focus()
+	await get_tree().process_frame
+	get_child(i).grab_focus()
+
+
+func item_focus() -> void:
+	for i in get_child_count():
+		if get_child(i).has_focus():
+			focus_index = i
+			return
+	pass
+
+
+func on_inventory_changed() -> void:
+	var i = focus_index
+	clear_inventory()
+	update_inventory(i)
+	pass
