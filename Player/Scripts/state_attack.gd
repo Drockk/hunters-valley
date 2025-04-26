0729b39ca4c8d86f30cb5d169589a89ac3953816
Bartosz Zielonka <batziell@gmail.com>
2025-04-26 22:06:13 +0200

    Enemy Item Drops

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -25,7 +25,9 @@ func Enter() -> void:
 	attacking = true
 	
 	await get_tree().create_timer(0.075).timeout
-	hurt_box.monitoring = true
+	if attacking:
+		hurt_box.monitoring = true
+	pass
 
 func Exit() -> void:
 	animation_player.animation_finished.disconnect(EndAttack)
