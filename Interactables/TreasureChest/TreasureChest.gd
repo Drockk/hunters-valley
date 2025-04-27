1b43f4677eaa47a28df13029e8069a76dd375400
Bartosz Zielonka <batziell@gmail.com>
2025-04-27 19:45:43 +0200

    Treasure Chest

---
+++ Interactables/TreasureChest/TreasureChest.gd
@@ -0,0 +1,77 @@
+@tool
+class_name TresureChest extends Node2D
+
+@export_category("Item")
+@export var item_data: ItemData: set = _set_item_data
+@export var quantity: int = 1: set = _set_quantity
+
+var is_open: bool = false
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var item_sprite: Sprite2D = $ItemSprite2D
+@onready var label: Label = $ItemSprite2D/Label
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var interact_area: Area2D = $Area2D
+
+
+func _ready() -> void:
+	_update_label()
+	_update_texture()
+	
+	if Engine.is_editor_hint():
+		return
+	
+	interact_area.area_entered.connect(_on_area_enter)
+	interact_area.area_exited.connect(_on_area_exit)
+	pass
+
+
+func _on_area_enter(_a: Area2D) -> void:
+	PlayerManager.interact_pressed.connect(player_interact)
+	pass
+
+
+func _on_area_exit(_a: Area2D) -> void:
+	PlayerManager.interact_pressed.disconnect(player_interact)
+	pass
+
+
+func player_interact() -> void:
+	if is_open == true:
+		return
+	
+	is_open = true
+	animation_player.play("open_chest")
+	if item_data and quantity > 0:
+		PlayerManager.INVENTORY_DATA.add_item(item_data, quantity)
+	else:
+		printerr("No Items in Chest!")
+		push_error("No Items in Chest! Chest name: ", name)
+	pass
+
+
+func _set_item_data(value: ItemData) -> void:
+	item_data = value
+	_update_texture()
+	pass
+
+
+func _set_quantity(value: int) -> void:
+	quantity = value
+	_update_label()
+	pass
+
+
+func _update_texture() -> void:
+	if item_data and sprite:
+		item_sprite.texture = item_data.texture
+	pass
+
+
+func _update_label() -> void:
+	if label:
+		if quantity <= 1:
+			label.text = ""
+		else:
+			label.text = "x" + str(quantity)
+	pass
